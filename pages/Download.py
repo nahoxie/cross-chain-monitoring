40da4fb32b22b51e565f7f6f1212aa50d0cb4912
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-20 08:34:50 -0400

    Update Download.py

--- pages/Download.py
+++ pages/Download.py
@@ -14,6 +14,11 @@ with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
     for file_name, data in csv.items():
         zip_file.writestr(file_name, data.encode("utf-8"))
 
+  st.header("Download")
+  csv =  {"route.csv": df_route.to_csv(index=False), 
+          "data_planned_dt.csv": df_data_planned_dt.to_csv(index=False),
+        "demand_data.csv": df_demand_pivot.to_csv(index=False)}
+
 # Display a download button for the zip file
 st.download_button(
     label="Download All DataFrames as Zip",
