2f0fda76df86abb889236943de3369425b670d10
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-20 10:29:07 -0400

    Create Demand.py

---
+++ pages/Demand.py
@@ -0,0 +1,17 @@
+# Libraries
+import altair as alt
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import streamlit as st
+import pandas as pd
+
+#Tabs    
+tab1, tab2 = st.tabs(["Annual Demand", "Monthly Demand"])
+
+with tab1:
+  st.header("Annual Demand")
+  st.
+with tab2:
+  st.header("Monthly Demand")
