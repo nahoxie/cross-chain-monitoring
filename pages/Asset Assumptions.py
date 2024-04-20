bc813f77c16827b200f0ae6e03119aa85c79ee51
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-20 12:02:12 -0400

    Update Asset Assumptions.py

--- pages/Asset Assumptions.py
+++ pages/Asset Assumptions.py
@@ -12,14 +12,13 @@ uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"],accept_multip
 st.header("Planned DT")
 #st.data_editor(df_data_planned_dt,num_rows="dynamic")
 if uploaded_file is  None:
-    df = []
-st.write(uploaded_file )
+
 
 # Check if a file was uploaded
 if uploaded_file is not None:
     for uploaded_file in uploaded_files:
         # Read each uploaded CSV file
-        df = pd.read_csv(uploaded_file)
+        df_csv = pd.read_csv(uploaded_file)
 
 new_header = df_csv.iloc[6] #grab the 6th row for the header
 df_csv = df_csv[7:] #take the data less the header row
