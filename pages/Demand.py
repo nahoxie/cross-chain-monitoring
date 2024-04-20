6dda1751d30a815ae436cc115642295f651e9455
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-20 10:36:44 -0400

    Update Demand.py

--- pages/Demand.py
+++ pages/Demand.py
@@ -20,10 +20,10 @@ with tab2:
   st.header("Monthly Demand")
 # Generate a sequence of dates for the next 18 months
 start_date = datetime.now().replace(day=1).date()
-date_range = {'DPP':["DPP1"],'Month':[start_date + relativedelta(months=i) for i in range(18)]}
+date_range = {'DPP':["DPP1"],'Month':['DPP',start_date + relativedelta(months=i) for i in range(18)]}
 
 # Create a DataFrame with the dates
-df_demand = pd.DataFrame(date_range, columns=['DPP','Month'])
+df_demand = pd.DataFrame(date_range, columns=['Month'])
 df_demand_pivot = df_demand.transpose()
 df_demand_pivot.columns = df_demand_pivot.iloc[0]
 df_demand_pivot = df_demand_pivot[1:]
