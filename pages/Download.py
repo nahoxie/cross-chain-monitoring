30c61fbb14e76b17525c8d6762b713427f9b3d92
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-20 08:33:43 -0400

    Update Download.py

--- pages/Download.py
+++ pages/Download.py
@@ -5,7 +5,9 @@ import pandas as pd
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import streamlit as st
-import pandas as pd
+import pandas as pd\
+import io
+import zipfile
 
 zip_buffer = io.BytesIO()
 with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
