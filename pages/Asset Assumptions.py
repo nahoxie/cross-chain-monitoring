8863aed3ba2fe3bad44cf9420dda27265ec5eb81
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-20 11:51:27 -0400

    Update Asset Assumptions.py

--- pages/Asset Assumptions.py
+++ pages/Asset Assumptions.py
@@ -7,13 +7,6 @@ from dateutil.relativedelta import relativedelta
 import streamlit as st
 import pandas as pd
 
-data_planned_dt ={"Asset":["OSW-HM","OSW-HM"],
-                  "Month":["April 2024","May 2024"],
-                  "Hours":[72,24]}
-st.set_page_config(page_title='Routing', page_icon=':bar_chart:', layout='wide')
-
-df_data_planned_dt=pd.DataFrame(data_planned_dt)
-
 st.header("Planned DT")
 #st.data_editor(df_data_planned_dt,num_rows="dynamic")
 
@@ -22,12 +15,14 @@ uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
 # Check if a file was uploaded
 if uploaded_file is not None:
     # Read the CSV file
-    df = pd.read_csv(uploaded_file)
+    df_csv = pd.read_csv(uploaded_file)
     #df = df.transpose().apply(pd.to_numeric, errors='coerce')
     # Display the DataFrame
-new_header = df.iloc[6] #grab the 6th row for the header
-df = df[7:] #take the data less the header row
-df.columns = new_header #set the header row as the df header
+new_header = df_csv.iloc[6] #grab the 6th row for the header
+df_csv = df_csv[7:] #take the data less the header row
+df_csv.columns = new_header #set the header row as the df header
+
+df= df.append(df_csv)
 
 metric = st.sidebar.multiselect('Select Metric',df.Date)
 filtered_df = df[df['Date'].isin(metric)]
