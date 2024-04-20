a01342eba8134205a6300a460bdc70705b323d7b
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-20 08:33:59 -0400

    Update Download.py

--- pages/Download.py
+++ pages/Download.py
@@ -5,7 +5,7 @@ import pandas as pd
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import streamlit as st
-import pandas as pd\
+import pandas as pd
 import io
 import zipfile
 
