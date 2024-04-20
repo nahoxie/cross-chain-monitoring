3b5a18a2301726ae29e4629c9fc01be36c937edf
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-20 12:42:31 -0400

    Update Routing.py

--- pages/Routing.py
+++ pages/Routing.py
@@ -15,27 +15,28 @@ import pandas as pd
 import io
 import zipfile
 
-# Global Variables
-theme_plotly = None # None or streamlit
-week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-# Config
-st.set_page_config(page_title='Routing', page_icon=':bar_chart:', layout='wide')
-
 data_route = {
-    "DPP": ["DPP1", "DPP2", "DPP3","DPP4"],
+    "MPP": ["MPP1", "MPP2", "MPP3","MPP4"],
     "Source": ["OSW-HM", "OSW-HM", "OSW-HM","OSW-HM"],
     "Sink": ["OSW-CM72", "OSW-88", "KIN-CM1","LOG-CM4"],
     "Active":[False,True,False,False]}
 
-
 my_global_routing_dataframe  = pd.DataFrame(data_route)
 
+# Config
+st.set_page_config(page_title='Routing', page_icon=':bar_chart:', layout='wide')
 
 st.header("Routing")
+filtered
 st.data_editor(my_global_routing_dataframe,num_rows="dynamic")
-st.first_filter = st.sidebar.multiselect('Select DPP',["DPP1","DPP2"])
-st.second_filter = st.sidebar.multiselect('Select Source',["OSW-HM"])
+st.first_filter = st.sidebar.multiselect('Select MPP',data_route.MPP)
+
+
+
+
+
+
+
 
 
 st.session_state["my_global_routing_dataframe"] = my_global_routing_dataframe
