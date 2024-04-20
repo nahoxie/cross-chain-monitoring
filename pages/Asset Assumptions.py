a3fcfbc3f00934a0b0938f960f9e02cef3ff12c0
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-20 08:20:33 -0400

    Update and rename 4_🔄_Swaps.py to Asset Assumptions.py

---
+++ pages/Asset Assumptions.py
@@ -0,0 +1,20 @@
+# Libraries
+import altair as alt
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import streamlit as st
+import pandas as pd
+
+data_planned_dt ={"Asset":["OSW-HM","OSW-HM"],
+                  "Month":["April 2024","May 2024"],
+                  "Hours":[72,24]}
+
+df_data_planned_dt=pd.DataFrame(data_planned_dt)
+
+
+
+st.header("Planned DT")
+st.data_editor(df_data_planned_dt,num_rows="dynamic")
+st.asset = st.sidebar.multiselect('Select Month',["April","May"])
