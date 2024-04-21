eabeda2a14b9fdcd19b86d899ec5fbe5bcf9068e
nahoxie <67507971+nahoxie@users.noreply.github.com>
2024-04-21 07:23:58 -0400

    Update Demand.py

--- pages/Demand.py
+++ pages/Demand.py
@@ -51,7 +51,7 @@ with tab3:
   df_demand_pivot_Specialties = df_demand_Specialties.transpose()
   df_demand_pivot_Specialties.columns = df_demand_pivot_Specialties.iloc[0]
   df_demand_pivot_Specialties = df_demand_pivot_Specialties[1:]
-  edited_demand_data_Specialties= st.data_editor(df_demand_pivot_Auto,num_rows="dynamic",,height=1002)
+  edited_demand_data_Specialties= st.data_editor(df_demand_pivot_Auto,num_rows="dynamic",height=1002)
   chart_demand_data_Specialties = edited_demand_data_Specialties.transpose().apply(pd.to_numeric, errors='coerce')
   st.dataframe(edited_demand_data_Specialties)
   st.bar_chart(chart_demand_data_Specialties)
